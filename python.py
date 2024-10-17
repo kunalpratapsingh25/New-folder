726817453d9f1af4bd78e4eaaeeb759e6089949a
kunalpratapsingh25 <kunalpratapsingh0987@gmail.com>
2024-10-17 22:39:45 +0530

    first commit

---
+++ python.py
@@ -0,0 +1,4 @@
+print("Welcome to the band generator name")
+city=input("in which city you live in ? \n")
+pet=input("what is the name of a pet?\n")
+print("your band name generator could be"+city+ pet )
